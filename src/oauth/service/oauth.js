6a253251c9e6acefc47cfb98819b75cfa4274bc1
shasharoman <shasharoman@gmail.com>
2016-05-30 21:04:07 +0800

    wechat enable dev mode add validate process

--- src/oauth/service/oauth.js
+++ src/oauth/service/oauth.js
@@ -1,5 +1,21 @@
-exports.wechat = wechat;
+var crypto = require('crypto');
+var _ = require('lodash');
 
-function wechat(req, res, callback) {
+exports.enableDevMode = enableDevMode;
+
+function enableDevMode(req, res) {
+    if (!validate(req.query.signature, req.query.timestamp, req.query.nonce)) {
+        res.send('validate failure');
+        return;
+    }
+
     res.send(req.query.echostr);
+}
+
+function validate(signature, timestamp, nonce) {
+    var token = global.wechatOauth.token;
+    var sorted = _.sortBy([token, timestamp, nonce]).join('');
+    var hash = crypto.createHash('sha1').update(sorted).digest('hex');
+
+    return hash === signature;
 }
